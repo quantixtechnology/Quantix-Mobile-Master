41c70d4109b20ffe8b629f32ac9a3da27b7ab3ca
Mukhtar Khan <mukhtarkhan143@gmail.com>
2026-05-24 23:16:21 +0530

    Remove shared/ from direct tracking (converting to submodule)

--- shared/lib/constants/app_constants.dart
+++
@@ -1,5 +0,0 @@
-class AppConstants {
-  static const int itemsPerPage = 20;
-  static const String defaultCurrency = 'PKR';
-  static const Duration animationDuration = Duration(milliseconds: 300);
-}
